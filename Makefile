e3cd2e72f459609f724dc7e1236160ea2aa93c97
Hugo Arregui <hugo.arregui@gmail.com>
2016-09-14 20:43:23 -0300

    WIP: update egg to new specification (#1)
    - bin support
    - str support

--- Makefile
+++ Makefile
@@ -5,13 +5,12 @@ module: clean
 	csc -X bind -c++ -s -j msgpack -o msgpack.so msgpack.scm
 	csc msgpack.import.scm -dynamic
 
-test : clean
+test: clean
 
-	csc -c tests/utils.scm
-	csc -X bind -c++ tests/utils.o tests/tests.scm -o run 
+	csc -X bind -c++ -I tests/ tests/tests.scm -o run
 	./run
 
-clean :
+clean:
 
 	rm -f tests/*.o *.o run *.c tests/*.c *.so msgpack.import.scm
 
--- msgpack-imple.scm
+++ msgpack-imple.scm
@@ -2,7 +2,7 @@
 ;;
 ;;  Copyright (c) 2013, Hugo Arregui
 ;;  All rights reserved.
-;;  
+;;
 ;;  Redistribution and use in source and binary forms, with or without
 ;;  modification, are permitted provided that the following conditions
 ;;  are met:
@@ -13,7 +13,7 @@
 ;;     documentation and/or other materials provided with the distribution.
 ;;  3. The name of the authors may not be used to endorse or promote products
 ;;     derived from this software without specific prior written permission.
-;;  
+;;
 ;;  THIS SOFTWARE IS PROVIDED BY THE AUTHORS ``AS IS'' AND ANY EXPRESS OR
 ;;  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
 ;;  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -25,7 +25,7 @@
 ;;  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 ;;  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-(use srfi-69 byte-blob numbers)
+(use srfi-69 byte-blob numbers srfi-1 matchable)
 (include "flonum-utils.scm")
 
 ;; limits
@@ -40,6 +40,7 @@
 (define int32_limit (- (- (expt 2 31) 1)))
 (define int64_limit (- (- (expt 2 63) 1)))
 (define fixed_raw_limit 31) ;in bytes
+(define raw8_limit (- (expt 2 8) 1))
 (define raw16_limit (- (expt 2 16) 1))
 (define raw32_limit (- (expt 2 32) 1))
 (define fixed_array_limit 15) ;in bytes
@@ -51,8 +52,8 @@
 
 ;; constants
 (define constants '((()      . #xc0)
-                    (#t      . #xc3) 
-                    (#f      . #xc2) 
+                    (#t      . #xc3)
+                    (#f      . #xc2)
                     (uint8   . #xcc)
                     (uint16  . #xcd)
                     (uint32  . #xce)
@@ -61,19 +62,24 @@
                     (int16   . #xd1)
                     (int32   . #xd2)
                     (int64   . #xd3)
+                    (str8    . #xd9)
+                    (str16   . #xda)
+                    (str32   . #xdb)
+                    (bin8    . #xc4)
+                    (bin16   . #xc5)
+                    (bin32   . #xc6)
                     (float   . #xca)
                     (double  . #xcb)
-                    (raw16   . #xda)
-                    (raw32   . #xdb)
                     (array16 . #xdc)
                     (array32 . #xdd)
                     (map16   . #xde)
                     (map32   . #xdf)))
 
 (define constant-repr-map (alist->hash-table constants))
-(define repr-constant-map 
-(alist->hash-table (map (lambda (entry) 
-                          (cons (cdr entry) (car entry))) constants)))
+
+(define repr-constant-map
+  (alist->hash-table (map (lambda (entry)
+                            (cons (cdr entry) (car entry))) constants)))
 
 (define (read-byte/eof-error port)
 (let ((b (read-byte port)))
@@ -99,7 +105,7 @@
   (apply bitwise-ior
          (let loop ((shift_size (- (* size 8) 8)))
            (if (>= shift_size 0)
-             (cons (arithmetic-shift 
+             (cons (arithmetic-shift
                      (f (read-byte/eof-error port)) shift_size)
                    (loop (- shift_size 8)))
              '()))))
@@ -118,7 +124,7 @@
   (write-bytes port value size))
 
 (define (read-sint port size #!optional (mapper identity))
-  (mapper 
+  (mapper
     (- (+ (read-bytes port size (lambda (b) (- 255 b)))
           1))))
 
@@ -127,11 +133,11 @@
 
 (define (read-float port #!optional (mapper identity))
   (mapper (byte-blob->float (byte-blob-reverse (read-raw port 4)))))
-       
+
 (define (write-double port value)
   (write-raw port (double->byte-blob value) 8))
 
-(define (read-double port #!optional (mapper identity)) 
+(define (read-double port #!optional (mapper identity))
   (mapper (byte-blob->double (byte-blob-reverse (read-raw port 8)))))
 
 (define (write-raw port blob size)
@@ -150,6 +156,11 @@
         (loop (- size 1) (byte-blob-cons byte data)))
       (mapper (byte-blob-reverse data)))))
 
+(define (read-str port size #!optional (mapper identity))
+  (mapper (read-raw port size byte-blob->string)))
+
+(define read-bin read-raw)
+
 (define (write-array port array size)
   (assert (= (vector-length array) size))
   (let loop ((index 0))
@@ -157,7 +168,7 @@
       (pack port (vector-ref array index))
       (loop (+ index 1)))))
 
-(define (read-array port size #!optional (mapper identity)) 
+(define (read-array port size #!optional (mapper identity))
   (define array (make-vector size))
   (let loop ((index 0))
     (when (< index size)
@@ -171,7 +182,7 @@
                            (pack port k)
                            (pack port v))))
 
-(define (read-map port size #!optional (mapper identity)) 
+(define (read-map port size #!optional (mapper identity))
   (define table (make-hash-table #:size size))
   (let loop ((size size))
     (if (> size 0)
@@ -187,7 +198,7 @@
 (define (fixed-sint? value)
   (= (bitwise-and #xe0 value) #xe0))
 
-(define (fixed-raw? value)
+(define (fixed-str? value)
   (= (bitwise-and #xe0 value) #xa0))
 
 (define (fixed-array? value)
@@ -246,25 +257,46 @@
   (write-header port 'double)
   (write-double port value))
 
-(define (pack-raw port value)
-  (assert (byte-blob? value))
-  (let ((size (byte-blob-length value)))
-    (cond ((= size 0)
-           #f) ;do nothing
-          ((<= size fixed_raw_limit)
+(define (pack-str port value)
+  (assert (string? value))
+  (let* ((blob (string->byte-blob value))
+         (size (byte-blob-length blob)))
+    (cond ((<= size fixed_raw_limit)
            (let ((header (bitwise-ior #xa0 size)))
              (write-byte header port)
-             (write-raw port value size)))
+             (write-raw port blob size)))
+          ((<= size raw8_limit)
+           (write-header port 'str8)
+           (write-uint port size 1)
+           (write-raw port blob size))
           ((<= size raw16_limit)
-           (write-header port 'raw16)
+           (write-header port 'str16)
+           (write-uint port size 2)
+           (write-raw port blob size))
+          ((<= size raw32_limit)
+           (write-header port 'str32)
+           (write-uint port size 4)
+           (write-raw port blob size))
+          (#t
+           (out-of-limit-error 'str value)))))
+
+(define (pack-bin port value)
+  (assert (byte-blob? value))
+  (let ((size (byte-blob-length value)))
+    (cond ((<= size raw8_limit)
+           (write-header port 'bin8)
+           (write-uint port size 1)
+           (write-raw port value size))
+          ((<= size raw16_limit)
+           (write-header port 'bin16)
            (write-uint port size 2)
            (write-raw port value size))
           ((<= size raw32_limit)
-           (write-header port 'raw32)
+           (write-header port 'bin32)
            (write-uint port size 4)
            (write-raw port value size))
           (#t
-           (out-of-limit-error 'raw value)))))
+           (out-of-limit-error 'bin value)))))
 
 (define (pack-array port value)
   (let ((size (vector-length value)))
@@ -313,11 +345,11 @@
           ((flonum? value)
            (pack-double port value))
           ((blob? value)
-           (pack-raw port (blob->byte-blob value)))
+           (pack-bin port (blob->byte-blob value)))
           ((byte-blob? value)
-           (pack-raw port value))
+           (pack-bin port value))
           ((string? value)
-           (pack-raw port (string->byte-blob value)))
+           (pack-str port value))
           ((list? value)
            (pack-array port (list->vector value)))
           ((vector? value)
@@ -327,64 +359,41 @@
           (#t
            (error (format "I don't know how to handle: ~A" value))))))
 
-(define (raw->string/mapper e)
-  (if (byte-blob? e)
-    (byte-blob->string e)
-    e))
-
 (define (unpack port #!optional (mapper identity))
   (let ((value (read-byte port)))
     (if (eof-object? value)
       value
       (cond ((hash-table-exists? repr-constant-map value)
              (let ((constant (hash-table-ref repr-constant-map value)))
-               (cond ((eq? constant 'uint8)
-                      (read-uint port 1 mapper))
-                     ((eq? constant 'uint16)
-                      (read-uint port 2 mapper))
-                     ((eq? constant 'uint32)
-                      (read-uint port 4 mapper))
-                     ((eq? constant 'uint64)
-                      (read-uint port 8 mapper))
-                     ((eq? constant 'int8)
-                      (read-sint port 1 mapper))
-                     ((eq? constant 'int16)
-                      (read-sint port 2 mapper))
-                     ((eq? constant 'int32)
-                      (read-sint port 4 mapper))
-                     ((eq? constant 'int64)
-                      (read-sint port 8 mapper))
-                     ((eq? constant 'float)
-                      (read-float port mapper))
-                     ((eq? constant 'double)
-                      (read-double port mapper))
-                     ((eq? constant 'raw16)
-                      (let ((size (read-uint port 2)))
-                        (read-raw port size mapper)))
-                     ((eq? constant 'raw32)
-                      (let ((size (read-uint port 4)))
-                        (read-raw port size mapper)))
-                     ((eq? constant 'array16)
-                      (let ((size (read-uint port 2)))
-                        (read-array port size mapper)))
-                     ((eq? constant 'array32)
-                      (let ((size (read-uint port 4)))
-                        (read-array port size mapper)))
-                     ((eq? constant 'map16)
-                      (let ((size (read-uint port 2)))
-                        (read-map port size mapper)))
-                     ((eq? constant 'map32)
-                      (let ((size (read-uint port 4)))
-                        (read-map port size mapper)))
-                     (#t
-                      (mapper constant)))))
+               (match constant
+                      ('uint8   (read-uint port 1 mapper))
+                      ('uint16  (read-uint port 2 mapper))
+                      ('uint32  (read-uint port 4 mapper))
+                      ('uint64  (read-uint port 8 mapper))
+                      ('int8    (read-sint port 1 mapper))
+                      ('int16   (read-sint port 2 mapper))
+                      ('int32   (read-sint port 4 mapper))
+                      ('int64   (read-sint port 8 mapper))
+                      ('str8    (read-str port (read-uint port 1) mapper))
+                      ('str16   (read-str port (read-uint port 2) mapper))
+                      ('str32   (read-str port (read-uint port 4) mapper))
+                      ('bin8    (read-bin port (read-uint port 1) mapper))
+                      ('bin16   (read-bin port (read-uint port 2) mapper))
+                      ('bin32   (read-bin port (read-uint port 4) mapper))
+                      ('float   (read-float port mapper))
+                      ('double  (read-double port mapper))
+                      ('array16 (read-array port (read-uint port 2) mapper))
+                      ('array32 (read-array port (read-uint port 4) mapper))
+                      ('map16   (read-map port (read-uint port 2) mapper))
+                      ('map32   (read-map port (read-uint port 4) mapper))
+                      (else     (mapper constant)))))
             ((fixed-uint? value)
              (mapper value))
             ((fixed-sint? value)
              (mapper (- (byte-complement2 value))))
-            ((fixed-raw? value)
+            ((fixed-str? value)
              (let ((size (bitwise-and #x1f value)))
-               (read-raw port size mapper)))
+               (read-str port size mapper)))
             ((fixed-array? value)
              (let ((size (bitwise-and #x0f value)))
                (read-array port size mapper)))
